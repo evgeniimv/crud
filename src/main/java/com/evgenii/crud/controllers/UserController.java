46fbae76b1577b24fcf2f222d68846de7e1f5f6c
Evgenii Mukhamadiarov <evgenii_mukhamadiarov@epam.com>
2021-02-02 17:32:06 +0300

    couple clean-code

--- src/main/java/com/evgenii/crud/controllers/UserController.java
+++ src/main/java/com/evgenii/crud/controllers/UserController.java
@@ -3,13 +3,11 @@ package com.evgenii.crud.controllers;
 import com.evgenii.crud.aspect.Auditable;
 import com.evgenii.crud.aspect.EventCode;
 import com.evgenii.crud.dto.UserDto;
-import com.evgenii.crud.services.AuthorizationService;
 import com.evgenii.crud.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,7 +16,6 @@ import java.util.UUID;
 @RequestMapping(value = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
     private final UserService userService;
-    private final AuthorizationService authorizationService;
 
     @GetMapping("/{userId}")
     @Auditable(value = EventCode.CRUD_USERS_GET_BY_ID, params = {"userId"})
@@ -29,21 +26,13 @@ public class UserController {
     @GetMapping
     @Auditable(value = EventCode.CRUD_USERS_GET_ALL)
     public List<UserDto> getAllUser() {
-//        final String authorizationResponse = authorizationService.checkAuthorization("Some user");
-//        if (authorizationResponse == null) {
-//            return userService.getAllUsers();
-//        }
         return userService.getAllUsers();
     }
 
     @PostMapping
     @Auditable(value = EventCode.CRUD_USERS_CREATE, params = {"userDto:name", "userDto:email"})
     public UserDto createUser(@RequestBody UserDto userDto) {
-        final Instant a = Instant.now();
-        final UserDto user = userService.createUser(userDto);
-        final Instant b = Instant.now();
-        System.out.println("TIME: " + (b.toEpochMilli() - a.toEpochMilli()));
-        return user;
+        return userService.createUser(userDto);
     }
 
     @PutMapping("/{userId}")
